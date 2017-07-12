34bc7db02875e8f092dbabf5017b7438fb8d6f53
fuwujiaxx <312011150@qq.com>
2017-07-12 13:44:50 +0800

    2

--- src/WekaTest.java
+++ src/WekaTest.java
@@ -5,6 +5,10 @@ import java.io.FileReader;
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 import weka.classifiers.evaluation.NominalPrediction;
+import weka.classifiers.rules.DecisionTable;
+import weka.classifiers.rules.PART;
+import weka.classifiers.trees.DecisionStump;
+import weka.classifiers.trees.J48;
 import weka.core.FastVector;
 import weka.core.Instances;
 
@@ -74,7 +78,46 @@ public class WekaTest {
 		//Do 10-split cross validation
 		Instances[][] split = crossValidationSplit(data, 10);
 		
-		//
+		//Separate split into training and testing arrays
+		Instances[] trainingSplits = split[0];
+		Instances[] testingSplits = split[1];
+		
+		
+		//Use a set of classifiers
+		
+		Classifier[] models = {
+				new J48(), // a decision tree
+				new PART(),
+				new DecisionTable(), // decision table majority classifier
+				new DecisionStump() //one-level decision tree
+		};
+		
+		//Run for each model
+		for(int j = 0; j < models.length; j++){
+			
+			//Collect every group of predictions for current model in a FastVector
+			FastVector predictions = new FastVector();
+			
+			//for each training-testing split pair, train and test the classifier
+			for(int i = 0; i < trainingSplits.length; i++){
+				 Evaluation validation = classify(models[j] , trainingSplits[i] , testingSplits[i]);
+				 
+				 predictions.appendElements(validation.predictions());
+				 
+				 //Uncomment to see the summary for each training-testing pair.
+				 //System.out.println(models[j].toString());
+			}
+			
+			//Calculate overall accuracy of current classifier on all splits
+			double accuracy = calculateAccuracy(predictions);
+			
+			//Print current classifier's name and accuracy in a complicated,
+			//but nice-looking way.
+			System.out.println("Accuracy of" + models[j].getClass().getSimpleName() + ":"
+					 + String.format("%.2f%%" , accuracy)
+					 + "\n-----------------------------");
+			
+		}
 		
 	}
 }
